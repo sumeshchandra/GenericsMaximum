aa5e2d614192f1a4b30270d2c6f6fac214140dcf
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-07-24 00:13:23 +0530

    Refactor: Refactored to Generics method

--- src/com/Generics/Maximum.java
+++ src/com/Generics/Maximum.java
@@ -1,66 +1,31 @@
 package com.Generics;
 
 public class Maximum {
-	  /**
-     * here i have created a parameterized constructor having given parameters.
-     * First i have taken numberOne is maxNumber
-     * then i used compareTo method which will compare with numberTwo..and gradually compare with numberThree also.
-     *  numberOne
-     *  numberTwo
-     *  numberThree
-     *  return --> maxNumber
+	/**
+     * here i have used Generic functions.
+     * here E is the type of data.
+     * Gradually it will return the max value.
+     *  firstValue
+     *  secondValue
+     *  thirdValue
+     *  <E>
+     * @return maxValue
      */
-	
-    public static Integer findMaxNumber(Integer numberOne, Integer numberTwo, Integer numberThree) {
-        Integer maxNumber = numberOne;
-        if (numberTwo.compareTo(maxNumber) > 0) {
-            maxNumber = numberTwo;
+    public static <E extends Comparable<E>> E findMax(E firstValue, E secondValue, E thirdValue) {
+        E maxValue = firstValue;
+        if (secondValue.compareTo(maxValue) > 0) {
+            maxValue = secondValue;
         }
-        if (numberThree.compareTo(maxNumber) > 0) {
-            maxNumber = numberThree;
+        if (thirdValue.compareTo(maxValue) > 0) {
+            maxValue = thirdValue;
         }
-        return maxNumber;
-    }
-    
-    /**
-     * As above it'll work the same but having only float values.
-     *  numberOne
-     *  numberTwo
-     *  numberThree
-     * @return maxFNumber
-     */
-
-    public static Float findFloatMaxNumber(Float numberOne, Float numberTwo, Float numberThree) {
-        Float maxFNumber = numberOne;
-        if (numberTwo.compareTo(maxFNumber) > 0) {
-        	maxFNumber = numberTwo;
-        }
-        if (numberThree.compareTo(maxFNumber) > 0) {
-        	maxFNumber = numberThree;
-        }
-        return maxFNumber;
-    }
-    /**
-     * String numberOne
-     * String numberTwo
-     * String numberThree
-     * return Maximum of 3 string*/
-    
-    public static String findMaxofString(String numberOne, String numberTwo, String numberThree) {
-    	String maxString = numberOne;
-    	if (numberOne.compareTo(maxString) > 0) {
-    		maxString = numberTwo;
-    	}
-    	if (numberThree.compareTo(maxString) > 0) {
-    		maxString = numberThree;
-    	}
-    	return maxString;
+        return maxValue;
     }
     
     public static void main(String[] args) {
-		System.out.println(findMaxNumber(123,435,567));
-		System.out.println(findFloatMaxNumber(13.4f,34.5f,6f));
-		System.out.println(findMaxofString("galaxy", "universe", "world"));
+		System.out.println(findMax(123,435,567));
+		System.out.println(findMax(13.4f,34.5f,6f));
+		System.out.println(findMax("galaxy", "universe", "world"));
 	}
    
 }
